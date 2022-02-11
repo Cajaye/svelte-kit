d988269ad6c8d4427d451d0522f6e68fdab7c932
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-02-10 20:38:03 -0500

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>Aboute</title>
+</svelte:head>
+
+<h1>About page</h1>
+<p>Welcome</p>
---
+++ src/routes/about.ts
@@ -0,0 +1,7 @@
+/*
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+hook up with mongodb and run npx prisma db push
+*/
+
+export let data: string = '';
