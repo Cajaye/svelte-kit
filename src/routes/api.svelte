4350ba643874fa2bae877decd447db61ac45e2e4
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-02-14 22:02:22 -0500

    post route

--- src/routes/api.svelte
+++ src/routes/api.svelte
@@ -6,7 +6,40 @@
     <title>Users</title>
 </svelte:head>
 
+<main>
+    <form method="post" action="/api">
+        <label for="name">Name</label>
+        <input type="name" name="name">
+         <label for="email">Email</label>
+        <input type="email" name="email">
+         <label for="password">Password</label>
+        <input type="password" name="password">
+        <input type="submit" value="submit">
+    </form>
+</main>
+
 {#each users as user}
 <p>{user.name}</p>
 <p>{user.email}</p>
+<p>{user.id}</p>
+<p>{user.createdAt}</p>
 {/each}
+
+<style lang="scss">
+    main{
+        padding: 4rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    input{
+        margin-top: 3px;
+        margin-bottom: 2rem;
+        display: block;
+    }
+    input[type="submit"]{
+        cursor: pointer;
+        padding: 0.15rem;
+        text-transform: uppercase;
+    }
+</style>
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,4 +1,5 @@
-import { PrismaClient, User } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+import type { RequestEvent } from '@sveltejs/kit';
 
 const prisma = new PrismaClient();
 
@@ -10,6 +11,24 @@ export const get = async () => {
 		return {
 			status: 200,
 			body: { users }
+		};
+	} catch (error) {
+		console.error(error);
+		await prisma.$disconnect();
+	}
+};
+
+export const post = async ({ request }) => {
+	try {
+		const body = await request.json();
+		await prisma.$connect();
+		await prisma.user.create({
+			data: { ...body }
+		});
+
+		return {
+			status: 201,
+			body: { message: 'You were just added to our database' }
 		};
 	} catch (error) {
 		console.error(error);
